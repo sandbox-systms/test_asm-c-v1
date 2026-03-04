114a8cd13ef7661c5c2dde999980778241b70d58
sandbox <ghostnether28@gmail.com>
2026-03-04 20:39:53 -0300

    Function soma em asm

---
+++ src/func.asm
@@ -0,0 +1,13 @@
+global soma 
+
+section .text
+
+soma: mov rax, rdi
+    add, rax, rsi
+    ret 
+
+; ==================================================================================================
+;  O código acima define uma função chamada "soma" que recebe dois argumentos (rdi e rsi), 
+; realiza a soma desses argumentos e retorna o resultado em rax. A função é simples e eficiente, 
+; utilizando as instruções básicas de movimentação e adição.
+; ===============================================================================================
